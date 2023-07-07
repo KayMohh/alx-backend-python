7605b123da3d62f9ead369453ab9cbafaf78805b
Kaymohh <thrilltim@gmail.com>
2023-07-07 14:50:54 +0100

    nnon

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two string and returns a concatenated string"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function floor"""
+
+
+def floor(n: float) -> int:
+    """Takes two float n and returns floor of float"""
+    return int(n)
